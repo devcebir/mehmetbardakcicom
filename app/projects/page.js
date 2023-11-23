dac821155d27e347043405e71e2f74818210d591
devcebir <bardakcimehmetdev@gmail.com>
2023-11-23 13:10:58 +0300

    furniro project added for project

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,5 +1,6 @@
 import KitchenAppImage from "@/images/kitchen-app.png";
 import DappGateImage from "@/images/dappgate.png";
+import FurniroImage from "@/images/furniro.png";
 import Image from "next/image";
 import Link from "next/link";
 import { PiCodeBold } from "react-icons/pi";
@@ -7,20 +8,22 @@ import { PiCodeBold } from "react-icons/pi";
 export const metadata = {
   title: "Mehmet Bardakcı - Projeler",
 };
- 
+
 const page = () => {
   return (
     <div className="page h-screen flex flex-col justify-center max-sm:my-24">
-      <h2 className="font-bold text-6xl mb-10 max-xl:text-5xl max-sm:text-3xl">Projeler</h2>
+      <h2 className="font-bold text-6xl mb-10 max-xl:text-5xl max-sm:text-3xl">
+        Projeler
+      </h2>
       <div className="flex items-center gap-5 max-sm:flex-col max-sm:gap-8 ">
-        <div className="flex flex-col gap-4 justify-center p-3 hover:bg-zinc-800 border border-zinc-900 w-[384px] max-sm:w-[310px] max-xsm:w-[280px] rounded-2xl transform transition-all duration-300">
+        <div className="flex flex-col gap-4 justify-center p-3 hover:bg-zinc-800 border border-zinc-900 w-[500px] max-sm:w-[310px] max-xsm:w-[280px] rounded-2xl transform transition-all duration-300">
           <Link href="https://kitchen-app-alpha.vercel.app/" target="_blank">
             <Image
               src={KitchenAppImage}
               alt="kitchen app"
               width={400}
               height={400}
-              className="rounded-xl w-96 h-52 max-sm:w-80 max-sm:h-44 shadow-lg shadow-zinc-700"
+              className="rounded-xl w-80 h-40 max-sm:w-80 max-sm:h-44 shadow-lg shadow-zinc-700"
             />
           </Link>
 
@@ -51,14 +54,14 @@ const page = () => {
 
         {/*  */}
 
-        <div className="flex flex-col gap-4 justify-center p-3 hover:bg-zinc-800 border border-zinc-900 w-[400px] max-sm:w-[310px] max-xsm:w-[280px] rounded-2xl transform transition-all duration-300">
+        <div className="flex flex-col gap-4 justify-center p-3 hover:bg-zinc-800 border border-zinc-900 w-[500px] max-sm:w-[310px] max-xsm:w-[280px] rounded-2xl transform transition-all duration-300">
           <Link href="https://dapp-gate-clon.vercel.app/" target="_blank">
             <Image
               src={DappGateImage}
               alt="kitchen app"
               width={450}
               height={450}
-              className="rounded-xl w-96 h-52 max-sm:w-80 max-sm:h-44 shadow-lg shadow-zinc-700"
+              className="rounded-xl w-80 h-40 max-sm:w-80 max-sm:h-44 shadow-lg shadow-zinc-700"
             />
           </Link>
 
@@ -78,6 +81,42 @@ const page = () => {
 
             <Link
               href="https://github.com/devcebir/dapp-gate-clon"
+              target="_blank"
+              className="flex items-center justify-center gap-3 mt-3 max-sm:text-sm text-zinc-400 hover:text-white border-zinc-400 rounded-md hover:border-white transform transition-all duration-300 border-b-2 w-36 max-sm:w-32"
+            >
+              <PiCodeBold />
+              <p className="">Source Code</p>
+            </Link>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4 justify-center p-3 hover:bg-zinc-800 border border-zinc-900 w-[500px] max-sm:w-[310px] max-xsm:w-[280px] rounded-2xl transform transition-all duration-300">
+          <Link href="https://ecommerce-ui-tau.vercel.app/" target="_blank">
+            <Image
+              src={FurniroImage}
+              alt="kitchen app"
+              width={450}
+              height={450}
+              className="rounded-xl w-80 h-40 max-sm:w-80 max-sm:h-44 shadow-lg shadow-zinc-700"
+            />
+          </Link>
+
+          <div>
+            <h4 className="text-zinc-400 max-sm:text-sm">
+              NEXT.JS - TAİLWİND CSS
+            </h4>
+            <Link href="https://ecommerce-ui-tau.vercel.app/" target="_blank">
+              <h3 className="font-extrabold text-2xl max-sm:text-xl">
+                Furniro e-Commerce
+              </h3>
+            </Link>
+            <p className="text-zinc-300 max-sm:text-sm">
+              Furniro isimli Figma tasarımının Nextjs ve Tailwind CSS
+              kullanılılarak yapımlış bir kopyası.
+            </p>
+
+            <Link
+              href="https://github.com/devcebir/ecommerce-ui"
               target="_blank"
               className="flex items-center justify-center gap-3 mt-3 max-sm:text-sm text-zinc-400 hover:text-white border-zinc-400 rounded-md hover:border-white transform transition-all duration-300 border-b-2 w-36 max-sm:w-32"
             >

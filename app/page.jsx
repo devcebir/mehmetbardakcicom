f9f68ec55ee90af019f4f1a3dda326f64a360016
devcebir <bardakcimehmetdev@gmail.com>
2023-11-29 02:05:36 +0300

    update

--- app/page.jsx
+++ app/page.jsx
@@ -3,16 +3,12 @@ export const metadata = {
   description: "Mehmet Cardakcı - Frontend Developer",
 };
 
-import { FaReact } from "react-icons/fa";
-
 export default function page() {
   return (
     <div class="flex flex-col items-center justify-center h-screen gap-5 select-none">
       <p class="animated-gradient text-7xl font-semibold max-xl:text-6xl max-lg:text-5xl max-md:text-4xl max-sm:text-3xl text-center">
         Yakında yeniden yayında...
       </p>
-
-      <FaReact className="text-9xl max-sm:text-8xl font-semibold custom-icon text-[#051111b6]" />
     </div>
   );
 }

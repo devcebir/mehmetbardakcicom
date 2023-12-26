969c038e7e37e159533583a1ae5c01c7d8a4a77b
devcebir <bardakcimehmetdev@gmail.com>
2023-12-26 23:27:24 +0300

    update home page

--- app/page.jsx
+++ app/page.jsx
@@ -5,14 +5,22 @@ export const metadata = {
 
 export default function page() {
   return (
-    <div className="flex flex-col flex-1 h-screen px-52 pt-24 max-[1024px]:px-28 max-[768px]:px-5 max-[768px]:pt-12">
-      <p className="font-bold text-xl mb-2">Ana Sayfa</p>
-      <p className="leading-7">
-        Merhaba, ben Mehmet. 👋 Kodların arasında kaybolan, sadeliği seven bir
-        yazılım geliştiricisiyim. Aynı zamanda tarih meraklısıyım. Seninle
-        paylaşmak istediğim bu küçük dünyada, kodlarımın ve geçmişin izlerinin
-        arasında geziniyorum. Hoş geldin! 🙃
-      </p>
+    <div className="flex flex-col flex-1 h-screen px-96 pt-24 max-[1024px]:px-28 max-[768px]:px-5 max-[768px]:pt-12">
+      <div className="flex flex-col gap-5">
+        <p className="leading-7">
+          Selam! Ben Mehmet. 👋 İşte karşında, kodların ve tasarımın ardındaki
+          yüz. Ancak bu yüz, sadece kodlamadan ibaret değil. Bir kitap kapağının
+          ardında kaybolmayı, tarihin derinliklerine dalıp geçmişle sohbet
+          etmeyi ve hayatı mümkün olduğunca basitleştirmeyi seven biriyim,
+          sadelikten yanayım.
+        </p>
+
+        <p className="leading-7">
+          Bu köşede, hem kodlama dünyasındaki maceralarımdan hem de kitapların
+          ve tarihin beni götürdüğü yerlerden bahsedeceğim. Hem kodların hem de
+          kelimelerin içinde, sadece bir tık ötedeyim. 🚀
+        </p>
+      </div>
     </div>
   );
 }

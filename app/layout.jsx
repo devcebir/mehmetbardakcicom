6e78446c17cb7284c5c2ac0eeb9a53e806ce2ab2
devcebir <bardakcimehmetdev@gmail.com>
2023-12-31 11:42:45 +0300

    delete site

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,10 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
-import Providers from "./Providers";
-import Header from "@/components/header";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="tr">
-      <body
-        className={`flex max-[768px]:flex-col bg-white dark:bg-black text-black dark:text-white h-screen selection:bg-gray-300 dark:selection:bg-gray-700 ${GeistSans.className}`}
-      >
-        <Providers>
-          <Header />
-          {children}
-        </Providers>
-      </body>
+      <body className={`${GeistSans.className}`}>{children}</body>
     </html>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,14 @@
 export const metadata = {
   title: "Mehmet Bardakcı",
-  description: "I am a Frontend Developer",
 };
-
 export default function page() {
   return (
-    <div className="flex flex-col flex-1 h-screen ml-72 max-[768px]:ml-0 px-96 pt-24 max-[1568px]:px-64 max-[1024px]:px-28 max-[768px]:px-5 max-[768px]:pt-20">
-      <div className="flex flex-col gap-5">
-        <p className="leading-7">
-          Selam! Ben Mehmet. 👋 İşte karşında, kodların ve tasarımın ardındaki
-          yüz. Ancak bu yüz, sadece kodlamadan ibaret değil. Bir kitap kapağının
-          ardında kaybolmayı, tarihin derinliklerine dalıp geçmişle sohbet
-          etmeyi ve hayatı mümkün olduğunca basitleştirmeyi seven biriyim,
-          sadelikten yanayım.
-        </p>
-
-        <p className="leading-7">
-          Bu köşede, hem kodlama dünyasından hem de kitapların ve tarihin beni
-          götürdüğü yerlerden bahsedeceğim. Kodların ve kelimelerin içinde,
-          sadece bir tık ötedeyim. 🚀
+    <div className="body-div">
+      <div class="kodlananMetin">
+        <p className="max-[321px]:text-lg">
+          This site is being coded<span class="yananNokta">.</span>
+          <span class="yananNokta">.</span>
+          <span class="yananNokta">.</span>
         </p>
       </div>
     </div>
